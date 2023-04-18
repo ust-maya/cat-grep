82a61d329cfe5be47afdc000ad42548bc4fbaaee
Rebeccaa <rebeccaa@student.21-school.ru>
2023-04-19 00:58:38 +0700

    simple implementation of the "grep" utility

--- src/grep/grep.h
+++ src/grep/grep.h
@@ -2,19 +2,30 @@
 #define GREP_H
 
 #include <getopt.h>
+#include <regex.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define ARR_SIZE 1000
+
 struct _flag {
-  int e, i, v, c, l, n, h, s, f;
-  int count_patterns;
-  int count_regex_files;
-  char *patterns[100];
-  char *regex_filex[100];
+  int e, i, v, c, l, n, h, s, f, o;
+  int files_num;
+  int templates_num;
 };
 
-void parser_flags(struct _flag *flag, int argc, char **argv);
-void open_files(struct _flag flag, int argc, char **argv);
-void printing(struct _flag flag, FILE *file);
+struct _array {
+  char **template;
+  char **file_name;
+};
 
+void allocate_memory(struct _array *array);
+void free_memory(struct _array *array);
+void parsing(struct _flag flag, struct _array array, int argc, char **argv);
+void regex_file(char **argv, char *reg_file_name, struct _array *array,
+                struct _flag *flag);
+void openfiles(char **argv, struct _flag flag, struct _array array);
+void grep(struct _flag flag, struct _array array, FILE *pfile_name,
+          int index_file_name);
 #endif  // GREP_H
